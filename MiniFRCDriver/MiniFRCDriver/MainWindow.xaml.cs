68eaca967aa1c2376d5b9c283883bf18a7424e4a
BuildTools <unconfigured@null.spigotmc.org>
2019-05-15 06:44:21 -0400

    new stuff

---
+++ MiniFRCDriver/MiniFRCDriver/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace MiniFRCDriver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void portRefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Refresh Clicked");
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach(string port in ports)
+            {
+                portList.Items.Add(port);
+                Console.WriteLine(port);
+            }
+        }
+    }
+}
