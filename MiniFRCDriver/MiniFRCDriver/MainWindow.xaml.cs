ae3a7b96ed03d71dbd259248ea81ce4d9c5fd407
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2019-05-16 18:37:42 -0400

    working on minifrcdriver (wip)

--- MiniFRCDriver/MiniFRCDriver/MainWindow.xaml.cs
+++ MiniFRCDriver/MiniFRCDriver/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace MiniFRCDriver
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string currentPort = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +31,10 @@ namespace MiniFRCDriver
         private void portRefreshButton_Click(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("Refresh Clicked");
+
+            portList.Items.Clear();
+
+            Console.WriteLine(SerialPort.GetPortNames());
             string[] ports = SerialPort.GetPortNames();
 
             foreach(string port in ports)
@@ -36,6 +42,36 @@ namespace MiniFRCDriver
                 portList.Items.Add(port);
                 Console.WriteLine(port);
             }
+
+            if (ports.Length == 0)
+            {
+                portList.Items.Add("No Devices");
+            }
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (portList.SelectedItem != null)
+            {
+                currentPort = portList.SelectedItem.ToString();
+                portNameLabel.Content = portList.SelectedItem.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please select a port first.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settings = new SettingsWindow();
+            settings.Show();
+        }
+
+        private void consoleButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConsoleWindow console = new ConsoleWindow();
+            console.Show();
         }
     }
 }
---
+++ MiniFRCDriver/MiniFRCDriver/SettingsWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiniFRCDriver
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public string[] settingLines;
+        public string[] autoLines;
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveSetButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory+"settings.txt", settingsBox.Text);
+            settingLines = Regex.Split(settingsBox.Text, "\r\n|\r|\n");
+        }
+
+        private void saveAutoButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "autonomous.txt", autoBox.Text);
+            autoLines = Regex.Split(autoBox.Text, "\r\n|\r|\n");
+        }
+
+        private void loadSsetButton_Click(object sender, RoutedEventArgs e)
+        {
+            settingsBox.Text = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "settings.txt");
+            settingLines = Regex.Split(settingsBox.Text, "\r\n|\r|\n");
+        }
+
+        private void loadAutoButton_Click(object sender, RoutedEventArgs e)
+        {
+            autoBox.Text = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "autonomous.txt");
+            autoLines = Regex.Split(autoBox.Text, "\r\n|\r|\n");
+        }
+    }
+}
